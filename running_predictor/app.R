7a2b8b30e0640fd0a661371918085aaed53ad5c0
James Hare <james@jamesphare.org>
2020-06-25 19:12:16 -0400

    cleaned up formatting

--- running_predictor/app.R
+++ running_predictor/app.R
@@ -1,63 +1,57 @@
 library(shiny)
 library(lubridate)
 
-# Define UI for application 
-ui <- fluidPage(
-
-    # Application title
+# Define UI for application
+ui <- fluidPage(# Application title
     titlePanel("Race Pace Predictor"),
-
-    # Sidebar 
+    
+    # Sidebar
     sidebarLayout(
         sidebarPanel(
             textInput("distance",
-                        "Length of race",
-                        value = 10),
+                      "Length of race",
+                      value = 10),
             textInput("training",
                       "Distance run in previous 12 weeks",
                       value = 100),
             radioButtons("units",
-                         "Units", 
-                         c("Kilometers" = "kms",
-                         "Miles" = "miles"))
+                         "Units",
+                         c(
+                             "Kilometers" = "kms",
+                             "Miles" = "miles"
+                         ))
         ),
-
+        
         # Show the predicted race pace
-        mainPanel(
-            tags$h3(
-            textOutput("pace")),
-            tags$h3(
-            textOutput("finish")
-            ))
-    )
-)
+        mainPanel(tags$h3(textOutput("pace")),
+                  tags$h3(textOutput("finish")))
+    ))
 
 
 # Define server logic required calculate race pace
 server <- function(input, output) {
-    
     output$pace <- renderText({
-        
-        if(input$units == "kms"){
-            
+        if (input$units == "kms") {
             # generate pace based on input$distance and input$training from ui.R
-            race_pace <- 342.06054 + (2.808069 * as.numeric(input$distance)) - 
+            race_pace <-
+                342.06054 + (2.808069 * as.numeric(input$distance)) -
                 (0.17157 * as.numeric(input$training))
-        
+            
             race_pace <- round(race_pace, 0)
-        
-            paste("Predicted race pace: ", seconds_to_period(race_pace), 
-              " / km", sep = "")
+            
+            paste("Predicted race pace: ",
+                  seconds_to_period(race_pace),
+                  " / km",
+                  sep = "")
         }
         else{
-            
             #convert miles to kilometers
             
             km_distance <- as.numeric(input$distance) * 1.609344
             km_training <- as.numeric(input$training) * 1.609344
             
             # generate pace based on input$distance and input$training from ui.R
-            race_pace <- 342.06054 + (2.808069 * km_distance) - 
+            race_pace <- 342.06054 + (2.808069 * km_distance) -
                 (0.17157 * km_training)
             
             #convert kilometers back to miles and display results
@@ -66,37 +60,39 @@ server <- function(input, output) {
             
             race_pace <- round(race_pace, 0)
             
-            paste("Predicted race pace: ", seconds_to_period(race_pace), 
-                  " / mile", sep = "")
+            paste("Predicted race pace: ",
+                  seconds_to_period(race_pace),
+                  " / mile",
+                  sep = "")
             
         }
         
     })
     
     output$finish <- renderText({
-        
-        if(input$units == "kms"){
-        
-        # generate pace based on input$distance and input$training from ui.R
-        race_pace <- 342.06054 + (2.808069 * as.numeric(input$distance)) - 
-            (0.17157 * as.numeric(input$training))
-        race_pace <- round(race_pace, 0)
-        
-        #generate finish time based on pace and distance
-        finish_time <- round(race_pace * as.numeric(input$distance), 0)
-
-        paste("Predicted finish time: ", seconds_to_period(finish_time), 
-              sep = "")
-        
-        }else{
+        if (input$units == "kms") {
+            # generate pace based on input$distance and input$training from ui.R
+            race_pace <-
+                342.06054 + (2.808069 * as.numeric(input$distance)) -
+                (0.17157 * as.numeric(input$training))
+            race_pace <- round(race_pace, 0)
             
+            #generate finish time based on pace and distance
+            finish_time <-
+                round(race_pace * as.numeric(input$distance), 0)
+            
+            paste("Predicted finish time: ",
+                  seconds_to_period(finish_time),
+                  sep = "")
+            
+        } else{
             #convert miles to kilometers
             
             km_distance <- as.numeric(input$distance) * 1.609344
             km_training <- as.numeric(input$training) * 1.609344
             
             # generate pace based on input$distance and input$training from ui.R
-            race_pace <- 342.06054 + (2.808069 * km_distance) - 
+            race_pace <- 342.06054 + (2.808069 * km_distance) -
                 (0.17157 * km_training)
             
             #convert kilometers back to miles
@@ -105,15 +101,17 @@ server <- function(input, output) {
             
             # generate finish time based on pace and distance
             
-            finish_time <- round(race_pace * as.numeric(input$distance), 0)
+            finish_time <-
+                round(race_pace * as.numeric(input$distance), 0)
             
-            paste("Predicted finish time: ", seconds_to_period(finish_time), 
+            paste("Predicted finish time: ",
+                  seconds_to_period(finish_time),
                   sep = "")
             
         }
     })
-
+    
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
